569b646ff37b28c496931d3b92ad9b8bbbe9e9b9
coldcatzzz <acat1212@163.com>
2021-04-06 18:28:13 +0800

    change1

--- myDemo.py
+++ myDemo.py
@@ -1,8 +1,36 @@
-# #取列表最大值
-# numbers = [1, 2, 3, 4, 5, 99, 7, 8, 9, 10]
+#取列表最大值(1)
+# numbers = [2, 2, 3, 4, 5, 99, 7, 8, 9, 10]
 # max = 0
 # for ss in numbers:
 #     if ss > max:
 #         max = ss
 # print(max)
+
+#取列表最大值(2)
+# numbers = [2, 2, 666, 4, 5, 99, 7, 8, 9, 10]
+# max = numbers[0]
+# for ss in numbers:
+#     if ss >= max:
+#         max = ss
+# print(max)
+
+
+#取列表最小值
+# numbers = [2, 2, 3, 4, 5, 99, 7, 1, 9, 10]
+# min = numbers[0]
+# for ss in numbers:
+#     if ss <= numbers[0]:
+#         min = ss
+# print(min)
+
+#计算列表的长度
+# numbers = [1, 2, 3, 4, 5, 99, 7, 8, 9, 10]
+# length = 0
+# for ss in numbers:
+#     length += 1
+# print(length)
+
+
+
+
 
